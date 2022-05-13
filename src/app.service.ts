0290161d99d8a4554f4e3ccd9739b14f62265b31
Sarmad Saeed <sarmad.saeed@bp.com>
2022-05-13 18:10:40 +0500

    Create new endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,5 +4,10 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     return 'Hello World!';
-  }
+    }
+    getUserInformation(): any {
+        const obj = { firstname: 'Sarmad', lastname: 'Saeed', cellNo:'484654546' };
+
+        return JSON.parse(JSON.stringify(obj));
+    }
 }
